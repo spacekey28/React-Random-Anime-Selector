d1b724a1162597a365141e56dff4f78ee0547810
jazzed28 <peteson.me@gmail.com>
2018-06-08 23:48:42 +1200

    heroku deployed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: '[name].[chunkhash].js'
   },
   module: {
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: 
   [
-    new CleanWebpackPlugin('dist', {} ),
+    new CleanWebpackPlugin('public', {} ),
     new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css'
     }),
