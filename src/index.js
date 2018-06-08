f10f604d46cc221ad065e0efa37ee6986482723b
jazzed28 <peteson.me@gmail.com>
2018-06-08 14:44:31 +1200

    webpack setup with scss loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return <div>Hello World</div>
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+
 module.exports = {
   module: {
     rules: [
@@ -7,7 +14,30 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  devtool: 'source-map',
+  plugins: [htmlPlugin]
 }
